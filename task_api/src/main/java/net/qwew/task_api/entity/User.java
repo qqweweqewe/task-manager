b67185cd667f69f11d88d8fdb7d4a5cf6f743565
Qwewewewew <golubkovivan4@gmail.com>
2024-04-18 12:24:54 +0300

    added service package i forgot

---
+++ task_api/src/main/java/net/qwew/task_api/entity/User.java
@@ -0,0 +1,19 @@
+package net.qwew.task_api.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+import jakarta.persistence.GenerationType;
+
+@Entity
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String username;
+    private String password;
+    private String roles;
+}
---
+++ task_api/src/main/java/net/qwew/task_api/service/TaskService.java
@@ -0,0 +1,46 @@
+package net.qwew.task_api.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import net.qwew.task_api.entity.Task;
+import net.qwew.task_api.repository.TaskRepository;
+
+@Service
+public class TaskService {
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    public Task createTask(Task newTask) {
+        return taskRepository.save(newTask);
+    }
+
+    public List<Task> getTasks() {
+        return taskRepository.findAll();
+    }
+
+    public Task getTask(long id) {
+        return taskRepository.findById(id).get();
+    }
+
+    public Task markDone(long id) {
+        Task task = getTask(id);
+        task.setDone(true);
+        return taskRepository.save(task);
+    }
+
+    public Task deleteTask(long id) {
+        Task task = getTask(id);
+        taskRepository.delete(task);
+        return task;
+    }
+
+    public List<Task> removeDone() {
+        List<Task> removedTasks = taskRepository.findAllByDone(true);
+        taskRepository.deleteByDone(true);
+        return removedTasks;
+    }
+}
